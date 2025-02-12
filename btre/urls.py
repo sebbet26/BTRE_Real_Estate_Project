a6c0b339be17df0967439ad5e2dc9818f001739b
sebbet26 <sebbet26@bergen.org>
2025-02-12 11:09:33 -0500

    2/12 - Fixed inquiry form

--- btre/urls.py
+++ btre/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
     path('listings/', include('listings.urls')),
-    path('accounts/', include('accounts.urls'))
+    path('accounts/', include('accounts.urls')),
+    path('contacts/', include('contacts.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
